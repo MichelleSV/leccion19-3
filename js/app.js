028a78f190bab69ca73f027823e4ba979865211c
MichelleSV <seguilvilchezmichelle@gmail.com>
2016-09-05 00:34:02 -0500

    corrigiendo document write

--- js/app.js
+++ js/app.js
@@ -3,8 +3,8 @@ var letra = prompt("Ingresa una letra");
 var contador=0;
 for (var i = 0; i < palabra.length; i++) {
 	var letraComparar = palabra.charAt(i);
-	if (letra == letraComparar){
+	if (letra === letraComparar){
 		contador++;
-	};
-	document.write(contador);
+	}
 }
+document.write(contador);

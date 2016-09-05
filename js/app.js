6a3b55f0c72d128b15dfafbfea5a7fc6bb36b28b
MichelleSV <seguilvilchezmichelle@gmail.com>
2016-09-05 00:20:41 -0500

    actualizando js, agregando var contador

--- js/app.js
+++ js/app.js
@@ -1,11 +1,10 @@
 var palabra = prompt("Ingresa una palabra");
-var longitud = palabra.length;
-
-var letras = prompt("Ingresa una letra");
-var letra;
-
-for (var i = 0; i < longitud; i++) {
-	letra = palabra[i];
-	if (letra == letras)
-		document.write(letra.length);
+var letra = prompt("Ingresa una letra");
+var contador=0;
+for (var i = 0; i < palabra.length; i++) {
+	var letraComparar = palabra.charAt(i);
+	if (letra == letraComparar){
+		contador++;
+	};
+	document.write(contador);
 }
